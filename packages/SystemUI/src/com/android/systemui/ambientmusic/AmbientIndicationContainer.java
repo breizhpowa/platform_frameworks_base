38e46733003a03d9dec602413797a2a2208c545f
bzhpowa <f4bi1@free.fr>
2019-04-06 18:49:34 +0200

    Revert "Ambient music ticker: fix text scrolling on AoD"
    This reverts commit fcb0f5d5e5e9dc464d1b5f20436ad5c0db471c13.

--- packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
+++ packages/SystemUI/src/com/android/systemui/ambientmusic/AmbientIndicationContainer.java
@@ -84,17 +84,8 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
     }
 
     public void updateDozingState(boolean dozing) {
-        if (mDozing != dozing) {
-            mDozing = dozing;
-            if (isAod()) {
-                setTickerMarquee(true, false);
-            }
-        }
+        mDozing = dozing;
         mAmbientIndication.setVisibility(shouldShow() ? View.VISIBLE : View.INVISIBLE);
-    }
-
-    private boolean isAod() {
-        return DozeParameters.getInstance(mContext).getAlwaysOn() && mDozing;
     }
 
     private boolean shouldShow() {
@@ -105,7 +96,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
         boolean filtered = lockscreenManager.shouldHideNotifications(
                 lockscreenManager.getCurrentUserId()) || lockscreenManager.shouldHideNotifications(
                         mStatusBar.getMediaManager().getMediaNotificationKey());
-        return (mKeyguard || isAod())
+        return mKeyguard
                 && ((mDozing && (mInfoAvailable || mNpInfoAvailable))
                 || (!mDozing && mNpInfoAvailable && !mInfoAvailable)
                 || (!mDozing && mInfoAvailable && filtered));
@@ -118,7 +109,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
                 @Override
                 public void run() {
                     mText.setEllipsize(TruncateAt.MARQUEE);
-                    mText.setMarqueeRepeatLimit(2);
+                    mText.setMarqueeRepeatLimit(5);
                     boolean rtl = TextUtils.getLayoutDirectionFromLocale(
                             Locale.getDefault()) == View.LAYOUT_DIRECTION_RTL;
                     mText.setCompoundDrawables(rtl ? null : mAnimatedIcon, null, rtl ?
@@ -165,9 +156,11 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
 
         mInfoToSet = null;
 
-        // if we are already showing an Ambient Notification with track info,
-        // stop the current scrolling and start it delayed again for the next song
-        setTickerMarquee(true, true);
+        if (mKeyguard) {
+            // if we are already showing an Ambient Notification with track info,
+            // stop the current scrolling and start it delayed again for the next song
+            setTickerMarquee(true, true);
+        }
 
         if (!TextUtils.isEmpty(notificationText)) {
             mInfoToSet = notificationText;
@@ -184,7 +177,7 @@ public class AmbientIndicationContainer extends AutoReinflateContainer {
             boolean isAnotherTrack = (mInfoAvailable || mNpInfoAvailable)
                     && (TextUtils.isEmpty(mLastInfo) || (!TextUtils.isEmpty(mLastInfo)
                     && !mLastInfo.equals(mInfoToSet)));
-            if (!isAod() && mStatusBar != null
+            if (!DozeParameters.getInstance(mContext).getAlwaysOn() && mStatusBar != null
                     && isAnotherTrack) {
                 mStatusBar.triggerAmbientForMedia();
             }
